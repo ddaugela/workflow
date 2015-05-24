4d965435df895d5c272870dffffc3ecd9f7c8643
Darcy Daugela <DarcyD@SpectrumOptix.com>
2015-05-23 21:53:58 -0600

    Smart Remark added

--- WorkFlow/WorkFlow/Program.cs
+++ WorkFlow/WorkFlow/Program.cs
@@ -7,6 +7,7 @@ namespace WorkFlow
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("You have changed..");
             Console.ReadKey();
         }
     }
