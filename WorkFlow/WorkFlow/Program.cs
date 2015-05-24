36be916e557f87812f8be10b467f5b4f586fe52d
Darcy Daugela <DarcyD@SpectrumOptix.com>
2015-05-23 21:38:32 -0600

    Testing VS 2015 Git Integration Hello World

---
+++ WorkFlow/WorkFlow/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkFlow
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
