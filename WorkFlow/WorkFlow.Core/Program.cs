9ce570f3e9bc36873d4fe64af41849195c21bf61
Darcy Daugela <DarcyD@SpectrumOptix.com>
2015-05-24 09:37:56 -0600

    Make some changes while laptop made changes.

--- WorkFlow/WorkFlow.Core/Program.cs
+++ WorkFlow/WorkFlow.Core/Program.cs
@@ -9,6 +9,10 @@ namespace WorkFlow
             Console.WriteLine("Hello World");
             Console.WriteLine("You have changed..");
             Console.WriteLine("VS 2015 Git hub integration is awesome...thanks github");
+
+            Console.WriteLine("Change made on desktop, at same time as another change on laptop");
+            Console.WriteLine("Would like to also have tests here....");
+
             Console.ReadKey();
         }
     }
